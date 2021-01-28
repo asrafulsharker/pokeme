6ea7c6c5dd46d210e96c8088a36930f9b7e7febf
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2021-01-29 00:26:01 +0600

    add 6 file

--- tweets/models.py
+++ tweets/models.py
@@ -5,4 +5,4 @@ from django.db import models
 
 class Tweet(models.Model):
     content = models.TextField(blank=True, null=True)
-    image = models.FileField(upload_to='images/',blank=True)
+    image = models.FileField(upload_to='images/',blank=True, null=True)
--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
+from .models import Tweet
 # Create your views here.
+
+def home_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Hello World</h1>")
+
+
+def tweets_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift or Java or ios/Andriod
+    """
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+    except:
+        raise Http404    
+    return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}</h1>")
