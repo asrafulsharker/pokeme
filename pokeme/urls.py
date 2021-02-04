ef981192fa8212245f5a2eed66284bb4a084dd82
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2021-02-04 09:39:26 +0600

    add request

--- pokeme/urls.py
+++ pokeme/urls.py
@@ -2,10 +2,11 @@
 from django.contrib import admin
 from django.urls import path, re_path
 
-from tweets.views import home_view, tweets_detail_view
+from tweets.views import home_view, tweets_detail_view, tweet_list_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
     path('tweets/<int:tweet_id>',tweets_detail_view),
+    path('tweets', tweet_list_view),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,20 +1,46 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404,JsonResponse
 from django.shortcuts import render
 
 from .models import Tweet
 # Create your views here.
 
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "pages/home.html",context={}, status=200)
+
+
+def tweet_list_view(request, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift or Java or ios/Andriod
+    return json data
+    """
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    data ={
+        "isUser": False,
+        "response": tweets_list
+    }
+    return JsonResponse(data)
 
 
 def tweets_detail_view(request, tweet_id, *args, **kwargs):
     """
     REST API VIEW
     Consume by JavaScript or Swift or Java or ios/Andriod
+    return json data
     """
+
+    data = {
+        "id":tweet_id,
+    }   
+
+    status = 200
+
     try:
         obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
     except:
-        raise Http404    
-    return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}</h1>")
+        data['massage'] = "Not found"
+        status = 404
+
+    return JsonResponse(data, status=status)
